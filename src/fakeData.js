34458a59104ea4f2ec4d5259c88b5ac40fb6687d
Luke Turner <luke@lukedturner.com>
2020-10-27 10:18:20 +1100

    can open and close modal

--- src/fakeData.js
+++ src/fakeData.js
@@ -1,11 +1,11 @@
 const fakeData = [
   {
-    id: 1,
+    id: 0,
     title: 'Jazz routine',
     steps: ['step1', 'step2'],
   },
   {
-    id: 2,
+    id: 1,
     title: 'Rock routine',
     steps: ['step1', 'step2'],
   }
--- src/pages/RoutineList/index.js
+++ src/pages/RoutineList/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import Button from '@material-ui/core/Button'
+import {Button, Modal} from '@material-ui/core/'
 import fakeData from "./../../fakeData";
+
 // Hit the API / DB, get a list of users routines
 // map them out with the title amd a button to play or edit
 
@@ -11,26 +12,52 @@ import fakeData from "./../../fakeData";
 
 const RoutineList = () => {
   const [userRoutines, setUserRoutines] = useState(fakeData);
+  const [editModalVisible, setEditModalVisible] = useState(false);
+  const [currentlySelectedRoutine, setCurrentlySelectedRoutine] = useState(null);
   // Default value is the existing list
   const enterEditMode = id => {
-    console.log(id);
-    console.log(userRoutines[id]);
+    // Open modal, set current routine
+    setEditModalVisible(true);
+    setCurrentlySelectedRoutine(userRoutines[id]);
+  }
+  
+  const handleModalClose = () => {
+    console.log('closing?')
+    setEditModalVisible(false);
+    setCurrentlySelectedRoutine(null);
+  }
+
+  const List = () => {
+    return (
+      userRoutines.map(routine => {
+        return (
+         <>
+           <h1>{routine.title}</h1>
+           <h1>{routine.id}</h1>
+           <Button variant="contained">Play</Button>
+           <Button variant="contained" onClick={() => enterEditMode(routine.id)}>Edit</Button>
+         </>
+        )
+      })
+    )
   }
   return (
     <>
 
-     {userRoutines.map(routine => {
-       return (
-        <>
+     <List />
+     <Modal
+      open={editModalVisible}
+      onClose={handleModalClose}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    > 
+    <div>
+      {currentlySelectedRoutine && <h1>{currentlySelectedRoutine.title}</h1>}
+    <Button variant="contained" onClick={() => handleModalClose()}>Close</Button>
+    </div>
 
-          <h1>{routine.title}</h1>
-          <h1>{routine.id}</h1>
-          <Button variant="contained">Play</Button>
-          <Button variant="contained" onClick={() => enterEditMode(routine.id)}>Edit</Button>
-
-        </>
-       )
-     })}
+    
+  </Modal>
 
     </>
   )
