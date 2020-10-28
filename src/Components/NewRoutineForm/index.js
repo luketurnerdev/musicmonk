a8a36b063dfbea17df1bc6b0476624b42fa62056
Luke Turner <luke@lukedturner.com>
2020-10-28 14:01:42 +1100

    can add new text fields

--- src/Components/NewRoutineForm/index.js
+++ src/Components/NewRoutineForm/index.js
@@ -2,22 +2,39 @@ import React, {useState} from 'react';
 import {Button, FormControl, TextField} from '@material-ui/core';
 
 const NewRoutineForm = props => {
+
+  //A new routine must have at least 1 step, a title and an id
   const {discardRoutine, saveRoutine} = props;
   const [formData, setFormData] = useState({});
+  const [userSteps, setUserSteps] = useState(['heres step 1','and also step 2']);
 
   const handleFormUpdate = (field, value) => {
     console.log(field, value);
     setFormData({...formData, [field]:value})
+  }
+
+  const addStep = () => {
+    setUserSteps(steps => [...steps, ['new step']])
+  }
+
+  const mapSteps = () => {
+    return (
+      userSteps && userSteps.map((step, index) => {
+        return <TextField defaultValue={step} />
+      })
+    )
   }
   return (
     <div>
     {console.log(formData)}
       <FormControl>
         <TextField
-        label="Routine name"
-        onChange={e => handleFormUpdate('title', e.target.value)}
+          label="Routine name"
+          onChange={e => handleFormUpdate('title', e.target.value)}
         />
       </FormControl>
+      {mapSteps()}
+      <Button onClick={() => addStep()}>Add Step</Button>
       <Button onClick={() => saveRoutine(formData)}>Save</Button>
       <Button onClick={() => discardRoutine()}>Discard</Button>
     </div>
