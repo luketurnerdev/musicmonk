1fb833358995c5f59720d7dd5446251c94261210
Luke Turner <luke@lukedturner.com>
2020-11-24 10:25:29 +1100

    fix routing and add user profile info

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import RoutineList from "./../RoutineList"
-import AuthNav from "./../../Components/AuthNav";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import NavBar from "./../../Components/NavBar"
+import Loading from "./../../Components/Loading"
+import Dashboard from "./../Dashboard";
+import Profile from "./../Profile";
+
+
 const App = props => {
-  // If user is logged in (dashboard)
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <>
-      <AuthNav />
-      <RoutineList />
-    </>
-  )
-}
+    <div id="app" className="d-flex flex-column h-100">
+      <h1>MusicMonk (header)</h1>
+      <NavBar />
+      <div className="container flex-grow-1">
+        <Switch>
+          <Route path="/profile" component={Profile} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
 export default App;
---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+import RoutineList from "./../RoutineList";
+const Dashboard = () => (
+  <>
+    <h1>Welcome, user !</h1>
+    <RoutineList />
+  </>
+);
+
+export default Dashboard;
