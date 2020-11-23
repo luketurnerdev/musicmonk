489b5eb9cd3eeea07f8e24c8f6c0d6eb4805b779
Luke Turner <luke@lukedturner.com>
2020-11-23 20:20:02 +1100

    connect app to auth0 with login and out buttons

--- src/auth/auth0-provider-with-history.js
+++ src/auth/auth0-provider-with-history.js
@@ -3,8 +3,8 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const domain = "musicmonk.au.auth0.com";
+  const clientId = "RRYnmLrc7nhJ8A0Iiv6TRr4p8yxO5DI7";
 
   const history = useHistory();
 
--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import RoutineList from "./../RoutineList"
+import AuthNav from "./../../Components/AuthNav";
 const App = props => {
   // If user is logged in (dashboard)
-  return <RoutineList />
+  return (
+    <>
+      <AuthNav />
+      <RoutineList />
+    </>
+  )
 }
 export default App;
