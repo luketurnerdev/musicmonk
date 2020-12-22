168f4fc943f59438ca0c9ce3f624ef207a9477f0
Luke Turner <luke@lukedturner.com>
2020-12-22 17:20:49 +1100

    add working test get request to /users

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 import ChordGenerator from "./../../Components/ChordGenerator";
 import RoutineList from "./../RoutineList";
+import SignupForm from "../../Components/SignupForm";
 const Dashboard = props => {
   const {user} = props;
   return (
     <>
       {/* <ChordGenerator /> */}
       {/* <h1>Welcome, {user.nickname} !</h1> */}
-      <RoutineList />
+      {/* <RoutineList /> */}
+      <SignupForm />
     </>
   );
 };
