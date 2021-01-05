00a8911335aaba4c51cf75354505a4f9d1e1c4d4
Luke Turner <luke@lukedturner.com>
2021-01-05 13:25:02 +1100

    make copy desktop nav component

---
+++ src/Components/DesktopNav/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  headerGridContainer: {
+    width: '100%',
+    margin: '30px 0'
+  },
+  menuGrid: {
+    textAlign: 'center',
+  },
+  exitGrid: {
+    textAlign: 'center',
+  },
+  titleGrid: {
+    textAlign: 'center',
+  },
+}
+
+export default styles;
