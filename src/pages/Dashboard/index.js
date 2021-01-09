4243c9f0a396a84608841986e7c52fdcc8ccb8cf
Luke Turner <luke@lukedturner.com>
2021-01-09 14:37:24 +1100

    add deploy script and header to dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,32 @@
 import React from "react";
 import RoutineList from "./../RoutineList";
+import {Typography} from '@material-ui/core';
 import NavBar from "./../../Components/NavBar";
 import {Grid} from "@material-ui/core";
 import {withStyles} from "@material-ui/styles";
 import styles from "./styles";
+
 const Dashboard = props => {
   const {user, classes} = props;
   return (
+  <>
+    <div className={classes.titleBar}>
+      <NavBar className={classes.navBarRoot} /> 
+      <Typography variant="subtitle" className={classes.title}>
+        MusicMonk 
+      </Typography>
+
+    </div>
     <Grid container className={classes.dashboardContainer}>
+
       <Grid item xs={1}>
-        <NavBar className={classes.navBarRoot} />
+        
       </Grid>
       <Grid item xs={11}>
         <RoutineList user={user} />
       </Grid>
     </Grid>
+  </>
   );
 };
 
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,6 +2,18 @@ const styles = {
   topMargin: {
     color: 'red'
   },
+  title: {
+    fontSize: '40px',
+    textAlign: 'center',
+    position: 'absolute',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    paddingTop: '15px',
+  },
+  titleBar: {
+    width: '100%',
+    height: '10vh',
+  }
 };
 
 export default styles;
