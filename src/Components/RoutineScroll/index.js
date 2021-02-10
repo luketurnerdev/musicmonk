eb39a1e7ef388eb7b25f084c4e14215ca34f3561
Luke Turner <luke@lukedturner.com>
2021-02-10 12:55:40 +1100

    implement back and forward buttons

--- src/Components/RoutineScroll/index.js
+++ src/Components/RoutineScroll/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Button} from '@material-ui/core';
+
 const RoutineScroll = props => {
   const {routines} = props;
-  console.log(routines)
+  const [currentRoutine, setCurrentRoutine] = useState(0);
+
+
+  const goForward = () => {
+    currentRoutine === routines.length-1
+      ? setCurrentRoutine(0)
+      : setCurrentRoutine(currentRoutine+1)
+  }
+  const goBack = () => {
+    currentRoutine === 0 
+      ? setCurrentRoutine(routines.length-1)
+      : setCurrentRoutine(currentRoutine-1)
+  }
   return (
     <div>
+      <Button onClick={() => goBack()}>Back</Button>
       <div>
-        scrolly
-        {routines[0].title}
-        {/* {routines[0].title} */}
+        {routines[currentRoutine].title}
+        Estimated time to complete: xyz
       </div>
+      <Button onClick={() => goForward()}>Forward</Button>
     </div>
   )
 }
@@ -22,6 +37,9 @@ export default RoutineScroll;
 
 // Goal 2: implement buttons to scroll left and right,
 // making sure that we can go <0 and > length to reset 
+// a) implement state for current routine
+// b) scroll left and right and alter array index as needed
+// c) implement useEffect to select last elemeny by default on load
 
 // Goal 3: get modals working
 
